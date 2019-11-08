ce66a6f48ebcd0577ad5b82fee337b80d04d72e0
Trần Công <nopain2110@gmail.com>
2019-11-08 18:27:27 +0700

    some tweaks

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 mkdir -p $HOME/.local/share/plasma/wallpapers/com.nerdyweekly.animated
 cp -rf ./* $HOME/.local/share/plasma/wallpapers/com.nerdyweekly.animated
- 
+rm -rf $HOME/.local/share/plasma/wallpapers/com.nerdyweekly.animated/.git
+rm -f $HOME/.local/share/plasma/wallpapers/com.nerdyweekly.animated/.gitignore
+rm -f $HOME/.local/share/plasma/wallpapers/com.nerdyweekly.animated/settings.png
+rm -f $HOME/.local/share/plasma/wallpapers/com.nerdyweekly.animated/README.md
+
 echo 'Done'
